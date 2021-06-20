9edab3c223974057a8f2c63fd9642fa83880c66c
M Nadeem Asghar <m.nadeem.asghar@hotmail.com>
2021-06-20 22:40:47 +0500

    Revert "some minor changes"
    This reverts commit 5afff6793ca3655e0ecf202d559548404b5a30e1.

--- payment.php
+++ payment.php
@@ -13,7 +13,7 @@ session_start();
 
 <h2>Receive Payments</h2>
 
-<form action=payment.php method=get>
+<form action=payment.php method=post>
 
 <table>
 <tr>
@@ -29,67 +29,70 @@ session_start();
 <?php
 
 
-if(isset($_GET["submit"]) || isset($_GET["slip"])){
-	$con = mysqli_connect("localhost","root","","iphs");
-	if(!$con){echo "Unable to Connect". mysqli_error();}
-	if($submit='View'){ 
-		if (isset($_GET["id"])) {
-			$id = $_GET["id"];
-		}
-		else  {
-			if (isset($_GET["slip"])) {
-			$id = $_GET["slip"];
-			}
-		}
+if(isset($_POST["submit"]) || isset($_GET["slip"])){
+$con = mysqli_connect("localhost","root","","iphs");
+if(!$con){echo "Unable to Connect". mysqli_error();}
+
+if($submit='View'){ 
+	if (isset($_POST["id"])) {
+		$id = $_POST["id"];
 	}
-
-	$slipdata = mysqli_query($con,"SELECT * FROM iphs.slips WHERE id = $id");
-
-	while($row = mysqli_fetch_array($slipdata)){
-
-		$faimlynum = $row["faimlynum"];
-		$total = $row["total"];
-		$month = $row["month"];
-
-		echo "<table><tr><td class=sprt >";
-		$paid = mysqli_fetch_array(mysqli_query($con,"SELECT SUM(amount) AS amount FROM iphs.payments WHERE srno = $id"));
-		$balance = $total - $paid["amount"];
-		echo "<table><tr><td>Sr. No:</td><td>".$id."</td></tr>";
-		echo "<tr><td>Family No:</td><td>".$faimlynum."</td><td><a href=viewpaymentbyfn.php?fn=".$faimlynum."&slip=".$id." >View All Slips by Family No</a></td></tr>";
-		echo "<tr><td>Fee Month:</td><td>".$month."</td></tr>";
-		echo "<tr><td>Slip Amount:</td><td>".$total."</td></tr>";
-		echo "<tr><td>Paid Amount:</td><td>".$paid["amount"]."</td></tr>";
-		echo "<tr><td>Balance Amount:</td><td>".$balance."</td></tr>";
-
-
-		echo "<form action=payment.php method=post>";
-		echo "<tr><td>Received Amount:</td><td><input type=hidden name=srno value=".$id."><input type=text name=amount></td></tr>";
-		echo "<tr><td></td><td><input type=submit name=Receive value=Receive></td></tr>";
-		echo "</form></table>";
+	else  {
+		if (isset($_GET["slip"])) {
+		$id = $_GET["slip"];
 	}
-	echo "</td><td>";
+	}}
+
+$slipdata = mysqli_query($con,"SELECT * FROM iphs.slips WHERE id = $id");
+
+while($row = mysqli_fetch_array($slipdata)){
+
+$faimlynum = $row["faimlynum"];
+$total = $row["total"];
+$month = $row["month"];
+
+echo "<table><tr><td class=sprt >";
+$paid = mysqli_fetch_array(mysqli_query($con,"SELECT SUM(amount) AS amount FROM iphs.payments WHERE srno = $id"));
+
+
+
+$balance = $total - $paid["amount"];
+echo "<table><tr><td>Sr. No:</td><td>".$id."</td></tr>";
+echo "<tr><td>Family No:</td><td>".$faimlynum."</td><td><a href=viewpaymentbyfn.php?fn=".$faimlynum."&slip=".$id." >View All Slips by Family No</a></td></tr>";
+echo "<tr><td>Fee Month:</td><td>".$month."</td></tr>";
+echo "<tr><td>Slip Amount:</td><td>".$total."</td></tr>";
+echo "<tr><td>Paid Amount:</td><td>".$paid["amount"]."</td></tr>";
+echo "<tr><td>Balance Amount:</td><td>".$balance."</td></tr>";
+
+
+echo "<form action=payment.php method=post>";
+echo "<tr><td>Received Amount:</td><td><input type=hidden name=srno value=".$id."><input type=text name=amount></td></tr>";
+echo "<tr><td></td><td><input type=submit name=submit value=Receive></td></tr>";
+echo "</form></table>";
+}
+echo "</td><td>";
 
 /*--------------------Payment History Section Start------------------------------------------*/
 echo "<h3>Payment History</h3>";
 echo "<table border=1><tr><td>Date</td><td>Amount</td></tr>";
 
-	$paymentdata = mysqli_query($con,"SELECT * FROM iphs.payments WHERE srno = $id");
+$paymentdata = mysqli_query($con,"SELECT * FROM iphs.payments WHERE srno = $id");
 
-	while($payment = mysqli_fetch_array($paymentdata)){
+while($payment = mysqli_fetch_array($paymentdata)){
 
-		echo "<tr><td>";
-		echo date("d-m-y",$payment["date"]);
-		echo "</td><td>";
-		echo $payment["amount"]."</td></tr>";
+echo "<tr><td>";
+echo date("d-m-y",$payment["date"]);
+echo "</td><td>";
+echo $payment["amount"]."</td></tr>";
 
-	}
+}
 echo "</table>";
 /*--------------------Payment History Section End------------------------------------------*/
 
 
 echo "</td></tr></table>";
 }
-if(isset($_POST["Receive"])){
+if(isset($_POST["submit"])){
 $con = mysqli_connect("localhost","root","","iphs");
 if(!$con){echo "Unable to Connect". mysqli_error();}
 
